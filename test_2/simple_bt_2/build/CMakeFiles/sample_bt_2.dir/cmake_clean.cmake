f27228bc11ba9252ef8c088bc9bec122ea128d06
Dybacap13 <91328259+Dybacap13@users.noreply.github.com>
2023-12-20 14:33:46 +0300

    Add files via upload

---
+++ test_2/simple_bt_2/build/CMakeFiles/sample_bt_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sample_bt_2.dir/test.cpp.o"
+  "sample_bt_2"
+  "sample_bt_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sample_bt_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test_2/simple_bt_2/build/CMakeFiles/simple_bt_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_bt_2.dir/test.cpp.o"
+  "simple_bt_2"
+  "simple_bt_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_bt_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
